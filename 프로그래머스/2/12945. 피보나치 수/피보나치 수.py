9c4c14717122ded9106ae201213772617f63906c
Yoon-Hee-Jae <140389762+Yoon-Hee-Jae@users.noreply.github.com>
2024-07-31 10:45:34 +0900

    [level 2] Title: 피보나치 수, Time: 488.37 ms, Memory: 456 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,16 @@
+def solution(n):
+    
+    MOD = 1234567
+    
+    fib = [0] * (n+1)
+    
+    fib[0] = 0
+    fib[1] = 1
+    
+    for i in range(2,n+1):
+        fib[i] = fib[i-1] + fib[i-2]
+    
+    answer = fib[n] % MOD
+    
+    return answer
+
